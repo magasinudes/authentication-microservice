54c20bb6e2630e036e84321a2964e769a34e0770
Justin Brûlotte <Justin2997@users.noreply.github.com>
2019-06-26 13:20:49 -0400

    Adding Post and good for CAS redirection (#5)
    * Change name for CI/CD
    
    * Added health point and unit test
    
    * Remove idea folder from git
    
    * Removed git conflict tags from file
    
    * Move to port 5001 and redirection port
    
    * Fix Test

--- src/main/java/com/magasinudes/authentication/AuthenticationController.java
+++ src/main/java/com/magasinudes/authentication/AuthenticationController.java
@@ -1,16 +1,10 @@
-package authentication;
+package com.magasinudes.authentication;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
-
     @RequestMapping("/health")
     public String health() {
         return "ok";
--- src/test/java/com/magasinudes/authentication/ApplicationTest.java
+++ src/test/java/com/magasinudes/authentication/ApplicationTest.java
@@ -1,4 +1,4 @@
-package authentication;
+package com.magasinudes.authentication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,13 +21,13 @@ public class ApplicationTest {
     private MockMvc mvc;
 
     @Test
-    public void helloGradle() throws Exception {
-        mvc.perform(get("/"))
+    public void testRedirection() throws Exception {
+        mvc.perform(get("/login"))
                 .andExpect(status().is3xxRedirection());
     }
 
     @Test
-    public void healthGradle() throws Exception {
+    public void testHealth() throws Exception {
         mvc.perform(get("/health"))
                 .andExpect(content().string("ok"));
     }
